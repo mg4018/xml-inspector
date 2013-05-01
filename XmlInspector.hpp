d8744fa11b242a4308ba6b509f82fff0bf51aadd
Przemek Mazurkiewicz <przemek.mazurkiewicz.2009@gmail.com>
2013-05-01 13:36:14 +0200

    That was a very lazy method.

--- XmlInspector.hpp
+++ XmlInspector.hpp
@@ -701,7 +701,7 @@ namespace Xml
 
 		NamespaceDeclarationType& NewNamespace();
 
-		bool CharsetEqual(const char32_t* upper, const char32_t* lower, std::size_t size);
+		bool CharsetEqual(const char32_t* charset);
 	public:
 		/**
 			@brief Initializes a new instance of the Inspector class.
@@ -4438,131 +4438,91 @@ namespace Xml
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf8Charset()
 	{
-		// 1234567890123456789
-		// UTF-8
-		// csUTF8
 		return (
-			CharsetEqual(U"UTF-8", U"utf-8", 5) ||
-			CharsetEqual(U"CSUTF8", U"csutf8", 6));
+			CharsetEqual(U"UTF-8") ||
+			CharsetEqual(U"csUTF8"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf16Charset()
 	{
-		// 1234567890123456789
-		// UTF-16
-		// csUTF16
 		return (
-			CharsetEqual(U"UTF-16", U"utf-16", 6) ||
-			CharsetEqual(U"CSUTF16", U"csutf16", 7));
+			CharsetEqual(U"UTF-16") ||
+			CharsetEqual(U"csUTF16"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf16BECharset()
 	{
-		// 1234567890123456789
-		// UTF-16BE
-		// csUTF16BE
 		return (
-			CharsetEqual(U"UTF-16BE", U"utf-16be", 8) ||
-			CharsetEqual(U"CSUTF16BE", U"csutf16be", 9));
+			CharsetEqual(U"UTF-16BE") ||
+			CharsetEqual(U"csUTF16BE"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf16LECharset()
 	{
-		// 1234567890123456789
-		// UTF-16LE
-		// csUTF16LE
 		return (
-			CharsetEqual(U"UTF-16LE", U"utf-16le", 8) ||
-			CharsetEqual(U"CSUTF16LE", U"csutf16le", 9));
+			CharsetEqual(U"UTF-16LE") ||
+			CharsetEqual(U"csUTF16LE"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf32Charset()
 	{
-		// 1234567890123456789
-		// UTF-32
-		// csUTF32
 		return (
-			CharsetEqual(U"UTF-32", U"utf-32", 6) ||
-			CharsetEqual(U"CSUTF32", U"csutf32", 7));
+			CharsetEqual(U"UTF-32") ||
+			CharsetEqual(U"csUTF32"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf32BECharset()
 	{
-		// 1234567890123456789
-		// UTF-32BE
-		// csUTF32BE
 		return (
-			CharsetEqual(U"UTF-32BE", U"utf-32be", 8) ||
-			CharsetEqual(U"CSUTF32BE", U"csutf32be", 9));
+			CharsetEqual(U"UTF-32BE") ||
+			CharsetEqual(U"csUTF32BE"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsUtf32LECharset()
 	{
-		// 1234567890123456789
-		// UTF-32LE
-		// csUTF32LE
 		return (
-			CharsetEqual(U"UTF-32LE", U"utf-32le", 8) ||
-			CharsetEqual(U"CSUTF32LE", U"csutf32le", 9));
+			CharsetEqual(U"UTF-32LE") ||
+			CharsetEqual(U"csUTF32LE"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsISO_8859_1_Charset()
 	{
-		// 1234567890123456789
-		// ISO-8859-1
-		// iso-ir-100
-		// ISO_8859-1
-		// latin1
-		// l1
-		// IBM819
-		// CP819
-		// csISOLatin1
 		return (
-			CharsetEqual(U"ISO-8859-1", U"iso-8859-1", 10) ||
-			CharsetEqual(U"ISO-IR-100", U"iso-ir-100", 10) ||
-			CharsetEqual(U"ISO_8859-1", U"iso_8859-1", 10) ||
-			CharsetEqual(U"LATIN1", U"latin1", 6) ||
-			CharsetEqual(U"L1", U"l1", 2) ||
-			CharsetEqual(U"IBM819", U"ibm819", 6) ||
-			CharsetEqual(U"CP819", U"cp819", 5) ||
-			CharsetEqual(U"CSISOLATIN1", U"csisolatin1", 11));
+			CharsetEqual(U"ISO-8859-1") ||
+			CharsetEqual(U"iso-ir-100") ||
+			CharsetEqual(U"ISO_8859-1") ||
+			CharsetEqual(U"latin1") ||
+			CharsetEqual(U"l1") ||
+			CharsetEqual(U"IBM819") ||
+			CharsetEqual(U"CP819") ||
+			CharsetEqual(U"csISOLatin1"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsISO_8859_2_Charset()
 	{
-		// 1234567890123456789
-		// ISO-8859-2
-		// iso-ir-101
-		// ISO_8859-2
-		// latin2
-		// l2
-		// csISOLatin2
 		return (
-			CharsetEqual(U"ISO-8859-2", U"iso-8859-2", 10) ||
-			CharsetEqual(U"ISO-IR-101", U"iso-ir-101", 10) ||
-			CharsetEqual(U"ISO_8859-2", U"iso_8859-2", 10) ||
-			CharsetEqual(U"LATIN2", U"latin2", 6) ||
-			CharsetEqual(U"L2", U"l2", 2) ||
-			CharsetEqual(U"CSISOLATIN2", U"csisolatin2", 11));
+			CharsetEqual(U"ISO-8859-2") ||
+			CharsetEqual(U"iso-ir-101") ||
+			CharsetEqual(U"ISO_8859-2") ||
+			CharsetEqual(U"latin2") ||
+			CharsetEqual(U"l2") ||
+			CharsetEqual(U"csISOLatin2"));
 	}
 
 	template <typename TCharactersWriter>
 	inline bool Inspector<TCharactersWriter>::IsWindows1250Charset()
 	{
-		// 1234567890123456789
-		// windows-1250
-		// cswindows1250
 		return (
-			CharsetEqual(U"WINDOWS-1250", U"windows-1250", 12) ||
-			CharsetEqual(U"CSWINDOWS1250", U"cswindows1250", 13));
+			CharsetEqual(U"windows-1250") ||
+			CharsetEqual(U"cswindows1250"));
 	}
 
 	template <typename TCharactersWriter>
@@ -4646,16 +4606,44 @@ namespace Xml
 	}
 
 	template <typename TCharactersWriter>
-	inline bool Inspector<TCharactersWriter>::CharsetEqual(
-		const char32_t* upper, const char32_t* lower, std::size_t size)
+	inline bool Inspector<TCharactersWriter>::CharsetEqual(const char32_t* charset)
 	{
+		static const unsigned char lower[256] =
+		{
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x0A, 0x00, 0x00, 0x0D, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+			0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+			0x40, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+			0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
+			0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+			0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
+			0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
+			0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
+			0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+			0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
+			0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
+			0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
+			0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
+			0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF
+		};
+
+		std::size_t length = 0;
+		const char32_t* pt = charset;
+		while (*pt != 0)
+		{
+			++length;
+			++pt;
+		}
+
 		// comparingName contains encoding name.
-		if (size != comparingName.size())
+		if (length != comparingName.size())
 			return false;
 
-		for (std::size_t i = 0; i < size; ++i)
+		for (std::size_t i = 0; i < length; ++i)
 		{
-			if (comparingName[i] != upper[i] && comparingName[i] != lower[i])
+			if (lower[static_cast<unsigned char>(comparingName[i])] !=
+				lower[static_cast<unsigned char>(charset[i])])
 				return false;
 		}
 
